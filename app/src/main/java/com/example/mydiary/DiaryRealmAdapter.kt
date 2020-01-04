fe1ee2057fa6642c0b69be9e25578b044ea48a7b
sasasa <masaakisaeki@gmail.com>
2020-01-04 16:09:39 +0900

    作業途中だがコンパイル通るところでcommit

---
+++ app/src/main/java/com/example/mydiary/DiaryRealmAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.mydiary
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import io.realm.OrderedRealmCollection
+import io.realm.RealmRecyclerViewAdapter
+
+class DiaryRealmAdapter(context: Context, data: OrderedRealmCollection<Diary>, autoUpdate: Boolean) : RealmRecyclerViewAdapter<Diary, DiaryRealmAdapter.DiaryViewHolder>(data, autoUpdate) {
+    var context: Context
+
+    init {
+        this.context = context
+    }
+
+    override fun onBindViewHolder(holder: DiaryViewHolder, position: Int) {
+        val diary = data?.get(position)
+        holder.title.setText(diary?.title)
+        holder.bodyText.setText(diary?.bodyText)
+        holder.date.setText(diary?.date)
+        if(diary?.image != null && diary?.image.size != 0) {
+            val bmp = MyUtils.getImageFromByte(diary?.image)
+            holder.photo.setImageBitmap(bmp)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DiaryViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
+        val holder = DiaryViewHolder(itemView)
+        return holder
+    }
+
+    inner class DiaryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var title: TextView
+        var bodyText: TextView
+        var date: TextView
+        var photo: ImageView
+        init {
+            title = itemView.findViewById<TextView>(R.id.title)
+            bodyText = itemView.findViewById<TextView>(R.id.body)
+            date = itemView.findViewById<TextView>(R.id.date)
+            photo = itemView.findViewById<ImageView>(R.id.diary_photo)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mydiary/MyUtils.kt
@@ -0,0 +1,30 @@
+package com.example.mydiary
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import java.io.ByteArrayOutputStream
+
+class MyUtils {
+    companion object {
+        fun getImageFromByte(bytes: ByteArray): Bitmap {
+            val opt = BitmapFactory.Options()
+            opt.inJustDecodeBounds = true
+            BitmapFactory.decodeByteArray(bytes, 0, bytes.size, opt)
+            var bitmapSize = 1
+            if(opt.outHeight * opt.outWidth > 500_000) {
+                val outSize = opt.outHeight * opt.outWidth / 500_000.0
+                bitmapSize = (Math.sqrt(outSize) + 1).toInt()
+            }
+            opt.inJustDecodeBounds = false
+            opt.inSampleSize = bitmapSize
+            val bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.size, opt)
+            return bmp
+        }
+
+        fun getByteFromImage(bmp: Bitmap): ByteArray {
+            val stream = ByteArrayOutputStream()
+            bmp.compress(Bitmap.CompressFormat.PNG, 100, stream)
+            return stream.toByteArray()
+        }
+    }
+}
